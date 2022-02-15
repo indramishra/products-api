a22765cb74af4fe5fa88fafaf3d8cdb7d3977022
indramishra <9indramishra@gmail.com>
2022-02-15 21:47:26 +0530

    initial commit

---
+++ api/assignment-project/src/main/java/com/indramishra/assignmentproject/AssignmentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.indramishra.assignmentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentProjectApplication.class, args);
+	}
+
+}
---
+++ api/assignment-project/src/test/java/com/indramishra/assignmentproject/AssignmentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indramishra.assignmentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
